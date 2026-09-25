d40eb5f508a9b4a144f20da7f21876767eeab8f9
tiagogomes1214-lab <tiagogomes1214@gmail.com>
2026-09-25 11:07:00 -0300

    buscador multi-loja: adiciona lojas/__init__.py

---
+++ lojas/__init__.py
@@ -0,0 +1,17 @@
+from .amazon import buscar_amazon
+from .aliexpress import buscar_aliexpress
+from .kabum import buscar_kabum
+from .mercado_livre import buscar_mercado_livre
+from .pichau import buscar_pichau
+from .terabyte import buscar_terabyte
+from .shopee import buscar_shopee
+
+__all__ = [
+    "buscar_amazon",
+    "buscar_aliexpress",
+    "buscar_kabum",
+    "buscar_mercado_livre",
+    "buscar_pichau",
+    "buscar_terabyte",
+    "buscar_shopee",
+]
